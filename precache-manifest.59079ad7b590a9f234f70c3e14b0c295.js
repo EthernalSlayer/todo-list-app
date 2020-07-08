9369decba637ecff99721fa090dc727122a20f93
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-07-08 22:20:24 +0200

    Updates

---
+++ precache-manifest.59079ad7b590a9f234f70c3e14b0c295.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e1f721891bb2d8d02244d908d67377",
+    "url": "/todo-list-app/index.html"
+  },
+  {
+    "revision": "fc6f4ac045013776db2f",
+    "url": "/todo-list-app/static/css/main.a4ea03fa.chunk.css"
+  },
+  {
+    "revision": "227b9f53b72ee4229cc3",
+    "url": "/todo-list-app/static/js/2.f469225c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-app/static/js/2.f469225c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6f4ac045013776db2f",
+    "url": "/todo-list-app/static/js/main.dfec4530.chunk.js"
+  },
+  {
+    "revision": "fc1ebefe855e12ff14a2",
+    "url": "/todo-list-app/static/js/runtime-main.d4521ec4.js"
+  },
+  {
+    "revision": "02464a71c93b4ed6a6ed1ed056c18481",
+    "url": "/todo-list-app/static/media/wood-pattern.02464a71.png"
+  }
+]);
